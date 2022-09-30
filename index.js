95789c3e3fed2d2843bb9ba678bbec0fa3479f7e
karan0987 <64898084+karan0987@users.noreply.github.com>
2022-09-30 14:46:17 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {Telegraf} = require('telegraf')
 const express = require("express");
 var app = express()
-app.launch()
+app.listen()
 var router = express.Router()
 app.use('/',router)
 var bot = new Telegraf('5783478127:AAG3HrfXaxI2ZFDZBAixnSku1N7Nnr1BYpY')
