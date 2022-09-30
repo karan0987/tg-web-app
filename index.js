26885b1432f168d8fd3d3506070e112047fe3460
Karan786-dev <81469693+Karan786-dev@users.noreply.github.com>
2022-09-30 14:00:33 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+const {Telegraf} = require('telegraf')
+var bot = new Telegraf('5783478127:AAG3HrfXaxI2ZFDZBAixnSku1N7Nnr1BYpY')
+bot.launch()
+console.log('Bot Launched.....')
+
+bot.command('start',(ctx)=>{
+  ctx.reply('Your Web App 👇👇',{
+    reply_markup : {
+      inline_keyboard : [
+        [{text:'Open Web App',web_app:{
+          url : 'https://karan786-dev.github.io/tg-web-app/'
+        }}]
+        ]
+    }
+  })
+})
